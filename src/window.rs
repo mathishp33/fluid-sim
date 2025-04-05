09589594dc6cae940f55306008439b85686cfe54
mathshp33 <maths33hp@gmail.com>
2025-04-05 23:19:46 +0200

    v2

---
+++ src/window.rs
@@ -0,0 +1,119 @@
+use std::time::Instant;
+use minifb::{Window, WindowOptions};
+
+mod fluid_sim;
+
+
+pub struct FluidWindow {
+    pub width: usize,
+    pub height: usize,
+    pub fps: i32,
+    pub last_update: Instant,
+    pub particle_radius: usize,
+    pub precision : usize,
+    pub window: Window,
+}
+
+impl FluidWindow {
+    pub fn new(width: usize, height: usize, fps: i32, particle_radius: usize, precision: usize) -> Self {
+        FluidWindow {
+            width,
+            height,
+            fps,
+            last_update: std::time::Instant::now(),
+            particle_radius,
+            precision,
+            window: Window::new(
+                "Fluid Simulation", 
+                width,
+                height, 
+                WindowOptions {
+                    resize: false,
+                    ..WindowOptions::default()
+                })
+                .unwrap_or_else(|e| {
+                    panic!("Unable to create window: {}", e);
+                }),
+        }
+    }
+
+    pub fn run(&mut self) {
+        let mut fluid = fluid_sim::Fluid::new((self.width / self.precision) as usize, (self.height / self.precision) as usize);
+        fluid.density.fill(0.0);
+        fluid.velocity_x.fill(0.0);
+        fluid.velocity_y.fill(0.0);
+    
+        let mut mouse_pos0 = (0, 0);
+    
+        while self.window.is_open() && !self.window.is_key_down(minifb::Key::Escape) {
+            let now = std::time::Instant::now();
+    
+            if now.duration_since(self.last_update) >= std::time::Duration::from_secs_f64(1.0f64 / self.fps as f64) {
+                let mut buffer: Vec<u32> = vec![0; self.width * self.height];
+                self.last_update = now;
+                
+                self.window.update();
+                let (mouse_x, mouse_y) = self.window.get_mouse_pos(minifb::MouseMode::Clamp).unwrap_or((0.0, 0.0));
+                let mouse_x = mouse_x as usize;
+                let mouse_y = mouse_y as usize;
+                if self.window.get_mouse_down(minifb::MouseButton::Left) {
+                    for dx in -(self.particle_radius as isize)..=(self.particle_radius as isize) {
+                        for dy in -(self.particle_radius as isize)..=(self.particle_radius as isize) {
+                            let nx = (mouse_x as isize + dx) as usize;
+                            let ny = (mouse_y as isize + dy) as usize;
+    
+                            if nx < self.width && ny < self.height {
+                                let distance_squared = (dx * dx + dy * dy) as f32;
+                                if distance_squared < (self.particle_radius * self.particle_radius) as f32 {
+                                    fluid.density[(nx / self.precision, ny / self.precision)] = 1.0;
+                                    fluid.velocity_x[(nx / self.precision, ny / self.precision)] = 0.0;
+                                    fluid.velocity_y[(nx / self.precision, ny / self.precision)] = 0.0;
+                                }
+                            }
+                        }
+                    }
+                }
+                if self.window.get_mouse_down(minifb::MouseButton::Right) {
+                    for dx in -(self.particle_radius as isize)..=(self.particle_radius as isize) {
+                        for dy in -(self.particle_radius as isize)..=(self.particle_radius as isize) {
+                            let nx = (mouse_x as isize + dx) as usize;
+                            let ny = (mouse_y as isize + dy) as usize;
+    
+                            if nx < self.width && ny < self.height {
+                                let distance_squared = (dx * dx + dy * dy) as f32;
+                                if distance_squared < (self.particle_radius * self.particle_radius) as f32 {
+                                    fluid.velocity_x[(nx / self.precision, ny / self.precision)] = (mouse_x as f64 - mouse_pos0.0 as f64) * 0.5f64;
+                                    fluid.velocity_y[(nx / self.precision, ny / self.precision)] = (mouse_y as f64 - mouse_pos0.1 as f64) * 0.5f64;
+                                }
+                            }
+                        }
+                    }
+                    mouse_pos0 = (mouse_x, mouse_y);
+                } else {
+                    mouse_pos0 = (mouse_x, mouse_y);
+                }
+                
+    
+                fluid.diffusion();
+                fluid.advection();
+    
+                for y in 0..fluid.height {
+                    for x in 0..fluid.width {
+                        let color = (fluid.get_density(x, y) * 255.0f64) as i32;
+                        let u32_color = ((color as u32) << 16) | ((color as u32) << 8) | (color as u32);
+                        for i in 0..self.precision {
+                            for j in 0..self.precision {
+                                let buffer_index = ((y * self.precision + j) * self.width + (x * self.precision + i)) as usize;
+                                buffer[buffer_index] = u32_color; 
+                            }
+                        }
+                    }
+                }
+                self.window.update_with_buffer(&buffer, self.width, self.height).unwrap();
+            } else {
+                std::thread::sleep(std::time::Duration::from_millis(1));
+            }
+        }
+    }
+}
+
---
+++ src/window/fluid_sim.rs
@@ -0,0 +1,78 @@
+use ndarray::Array2;
+
+pub struct Fluid {
+    pub width: usize,
+    pub height: usize,
+    pub density: Array2<f64>,
+    pub velocity_x: Array2<f64>,
+    pub velocity_y: Array2<f64>,
+}
+
+impl Fluid {
+    pub fn new(width: usize, height: usize) -> Self {
+        Fluid {
+            width,
+            height,
+            density: Array2::zeros((width, height)),
+            velocity_x: Array2::zeros((width, height)),
+            velocity_y: Array2::zeros((width, height)),
+        }
+
+    }
+
+    pub fn get_density(&mut self, x: usize, y: usize) -> f64 {
+        return self.density[(x, y)];
+    }
+
+    pub fn diffusion(&mut self) {
+        let k = 0.1; // diffusion rate
+        let mut new_density = self.density.clone();
+        for x in 1..self.width - 1 {
+            for y in 1..self.height - 1 {
+                new_density[(x, y)] += k * (
+                    self.density[(x + 1, y)] +
+                    self.density[(x - 1, y)] +
+                    self.density[(x, y + 1)] +
+                    self.density[(x, y - 1)] -
+                    4.0 * self.density[(x, y)]
+                );
+            }
+        }
+        self.density = new_density;
+    }
+
+    pub fn lerp(&mut self, a: f64, b: f64, k: f64) -> f64 {
+        a + k * (b - a)
+    }
+
+    pub fn advection(&mut self) {
+        let k = 0.5; //friction rate
+        let mut new_density = self.density.clone();
+        let mut new_velocity_x = self.velocity_x.clone();
+        let mut new_velocity_y = self.velocity_y.clone();
+        for x in 0..self.width {
+            for y in 0..self.height {
+                let x1 = x as f64 + self.velocity_x[(x, y)].round();
+                let y1 = y as f64 + self.velocity_y[(x, y)].round();
+                if x1 < 0.0 || y1 < 0.0 || x1 >= self.width as f64 - 1.0 || y1 >= self.height as f64 - 1.0 {
+                    continue;
+                }
+                let f = (x1 - self.velocity_x[(x, y)], y1 - self.velocity_y[(x, y)]);
+                let i = (f.0.floor(), f.1.floor());
+                let j = (f.0.fract(), f.1.fract());
+                if i.0 < 0.0 || i.1 < 0.0 || i.0 >= self.width as f64 - 1.0 || i.1 >= self.height as f64 - 1.0 {
+                    continue;
+                }
+                let z1 = self.lerp(self.density[(i.0 as usize, i.1 as usize)], self.density[(i.0 as usize + 1, i.1 as usize)], j.0);
+                let z2 = self.lerp(self.density[(i.0 as usize, i.1 as usize + 1)], self.density[(i.0 as usize + 1, i.1 as usize + 1)], j.0);
+
+                new_density[(x1 as usize, y1 as usize)] = self.lerp(z1, z2, j.1);
+                new_velocity_x[(x1 as usize, y1 as usize)] = self.velocity_x[(x, y)] * k;
+                new_velocity_y[(x1 as usize, y1 as usize)] = self.velocity_y[(x, y)] * k;
+            }
+        }
+        self.density = new_density;
+        self.velocity_x = new_velocity_x;
+        self.velocity_y = new_velocity_y;
+    }
+}
